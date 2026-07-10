475602e05680ae98e2fb4040ec50339fd8b77e78
SDB <91771020+Sagarkhatarkar12@users.noreply.github.com>
2026-07-10 21:45:58 +0530

    Final

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -6,6 +6,7 @@ import { FaBookReader } from "react-icons/fa";
 import { FaRegCalendarAlt } from "react-icons/fa";
 import { MdFolderCopy } from "react-icons/md";
 
+
 function Login() {
   const [formData, setFormData] = useState({
     email: "",
@@ -18,7 +19,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/api/login", {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
 
